5dd2481b3dc258b14d0b39c62a10f6d257d2e0db
Your Name <your-email@example.com>
2025-07-21 20:05:02 -0500

    test plugin

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,8 +1,29 @@
+import { storage } from "@vendetta/plugin";
 import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
+import { useProxy } from "@vendetta/storage";
 
-export default () => (
-    <FormText>
-        Hello, world!
-    </FormText>
-)
+const { FormInput, FormText } = Forms;
+
+// Typed plugin storage
+interface PluginStorage {
+    prefix?: string;
+}
+const typedStorage = storage as unknown as PluginStorage;
+
+export default () => {
+    useProxy(storage);
+
+    return (
+        <>
+            <FormText>Prefix for /echo command</FormText>
+            <FormInput
+                title="Prefix"
+                value={typedStorage.prefix ?? "[Echo]"}
+                onChange={(val) => {
+                    typedStorage.prefix = val;
+                }}
+                placeholder="[Echo]"
+            />
+        </>
+    );
+};
--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,47 @@
-import { logger } from "@vendetta";
+import { logger, commands } from "@vendetta";
 import Settings from "./Settings";
+import { storage } from "@vendetta/plugin";
+
+// Typed plugin storage
+interface PluginStorage {
+    prefix?: string;
+}
+const typedStorage = storage as unknown as PluginStorage;
+
+let unregisterCommand: () => void;
 
 export default {
     onLoad: () => {
-        logger.log("Hello world!");
+        logger.log("Plugin loaded.");
+
+        unregisterCommand = commands.registerCommand({
+            name: "echo",
+            displayName: "echo",
+            description: "Replies with your message",
+            displayDescription: "Replies with your message",
+            options: [
+                {
+                    name: "text",
+                    description: "Text to echo",
+                    displayName: "text",
+                    displayDescription: "Text to echo",
+                    type: 3, // STRING
+                    required: true,
+                },
+            ],
+            applicationId: "-1",
+            inputType: 1,
+            type: 1,
+            execute: ([text]) => ({
+                content: `${typedStorage.prefix ?? "[Echo]"} ${text.value}`,
+            }),
+        });
     },
+
     onUnload: () => {
-        logger.log("Goodbye, world.");
+        unregisterCommand?.();
+        logger.log("Plugin unloaded.");
     },
+
     settings: Settings,
-}
+};
