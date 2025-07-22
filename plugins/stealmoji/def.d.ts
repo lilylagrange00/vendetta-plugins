f07bbca22535598a3f95bf02c6cccdc4012f9c0a
Your Name <your-email@example.com>
2025-07-21 22:05:33 -0500

    undo

--- plugins/stealmoji/def.d.ts
+++
@@ -1,8 +0,0 @@
-declare interface EmojiNode {
-    frozenSrc: string;
-    type: string;
-    jumboable: boolean;
-    src: string;
-    alt: string;
-    id: string;
-}
--- plugins/stealmoji/lib/utils/lazy.ts
+++
@@ -1,4 +0,0 @@
-export default function lazy<T>(factory: () => T) {
-    let cache: T;
-    return () => (cache ??= factory());
-}
